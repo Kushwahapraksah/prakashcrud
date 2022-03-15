520d24833632f9435c85f0b32403592205b83610
admin <admin@CHETUIWK015>
2022-03-15 12:53:28 +0530

    Add project files.

---
+++ prakashcrud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="prakashcrud.MvcApplication" Language="C#" %>
---
+++ prakashcrud/Models/empclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prakashcrud.Models
+{
+    public class empclass
+    {
+        public int emp_id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+
+    }
+}
